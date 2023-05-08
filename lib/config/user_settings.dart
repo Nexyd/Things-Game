6e95db837810bf906f5697fcb66515cdd67a07cd
Daniel Morato <daniel.morato.20@gmail.com>
2023-05-08 23:05:27 +0200

    wip(settings): Init UserSettings from shared prefs data

--- lib/config/user_settings.dart
+++ lib/config/user_settings.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSettings {
+  static late final UserSettings? _instance;
+
   String name;
   Image avatar;
   Color primaryColor;
   Color textColor;
   Color backgroundColor;
   String language;
-
-  UserSettings({
-    required this.name,
-    required this.avatar,
-    required this.primaryColor,
-    required this.textColor,
-    required this.backgroundColor,
-    required this.language,
-  });
 
   UserSettings._privateConstructor({
     required this.name,
@@ -26,15 +20,36 @@ class UserSettings {
     required this.language,
   });
 
-  static final UserSettings _instance = UserSettings._privateConstructor(
-    name: "",
-    avatar: Image.asset("null"),
-    primaryColor: Colors.red,
-    textColor: Colors.white,
-    backgroundColor: Colors.grey.shade800,
-    // backgroundColor: Colors.yellow,
-    language: "en_ES",
-  );
+  static Future<void> init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final primary = _getColor(prefs.getString('primaryColor'));
+    final text = _getColor(prefs.getString('textColor'));
+    final background = _getColor(prefs.getString('backgroundColor'));
 
-  static UserSettings get instance => _instance;
+    _instance = UserSettings._privateConstructor(
+      name: prefs.getString('name') ?? "",
+      avatar: Image.asset(prefs.getString('avatar') ?? "null"),
+      primaryColor: primary ?? Colors.red,
+      textColor: text ?? Colors.white,
+      backgroundColor: background ?? Colors.grey.shade800,
+      language: prefs.getString('language') ?? "es_ES",
+    );
+  }
+
+  static Color? _getColor(String? hexString) {
+    try {
+      return Color(int.parse(hexString!, radix: 16));
+    } on Exception catch (error) {
+      debugPrint("### Exception parsing hex color: $error ###");
+      return null;
+    }
+  }
+
+  static UserSettings get instance {
+    if (_instance == null) {
+      UserSettings.init();
+    }
+
+    return _instance!;
+  }
 }
---
+++ lib/widget/color_picker.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorPicker extends StatefulWidget {
+  const ColorPicker({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
