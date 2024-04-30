6c3523c3f152f478ef8e66deff5b11e20ebb86eb
Daniel Morato <dmorato@bitban.com>
2024-04-30 15:47:55 +0200

    wip: Add basic db initialization skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:i18n_extension/i18n_widget.dart';
+import 'package:realm/realm.dart';
 import 'package:things_game/support/route_generator.dart';
 import 'package:things_game/screen/splash_screen.dart';
 
@@ -10,10 +11,32 @@ import 'cubit/game_cubit.dart';
 import 'cubit/room_cubit.dart';
 import 'support/logger.dart';
 
+const String MONGODB_URL = "https://realm.mongodb.com";
+
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   Logger.init(LoggerConfig(level: LoggerLevel.debug));
+
+  final app = App(AppConfiguration(
+    "application-0-ctycz",
+    baseUrl: Uri.parse(MONGODB_URL),
+  ));
+
+  // final currentUser = app.currentUser ?? (
+  //   await app.logIn(Credentials.anonymous())
+  // );
+
+  // final currentUser = await app.logIn(Credentials.anonymous());
+
+  // https://github.com/AgnelSelvan/Blogs/tree/main/realm_flutter_example
+  // runApp(
+  //   ChangeNotifierProvider(
+  //     create: (_) => QuotesNotifier(currentUser),
+  //     child: I18n(child: const ThingsGame()),
+  //   ),
+  // );
+
   runApp(I18n(child: const ThingsGame()));
 }
 
